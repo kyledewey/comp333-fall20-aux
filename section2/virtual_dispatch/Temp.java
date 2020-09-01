8cf9a8ec1af06a287f0326aec4184135b5f22907
Kyle Dewey <kyle.dewey@csun.edu>
2020-09-01 14:22:49 -0700

    added issues with this code

--- section2/virtual_dispatch/Temp.java
+++ section2/virtual_dispatch/Temp.java
@@ -5,6 +5,12 @@ public class Temp {
     public static boolean doesUserWantConsole(String[] args) { ... }
     public static String getDestinationFile(String[] args) { ... }
 
+    // Issues:
+    // 1.) userWantsConsole and destinationFile keep getting passed around
+    // 2.) userWantsConsole and destinationFile are closely related
+    // 3.) destinationFile doesn't make sense if userWantsConsole == true
+    // 4.) open/closes files each time, which is expensive
+    // 5.) we need to append to the end of the file, not overwrite it every time
     public static void writeThing(boolean userWantsConsole,
                                   String destinationFile,
                                   int thing) {
@@ -23,14 +29,14 @@ public class Temp {
                                     String[] args) {
         writeThing(userWantsConsole, destinationFile, 42);
     } // doComputation
-    
+
     // java Temp foo bar baz
     // args = { "foo", "bar", "baz" }
     public static void main(String[] args) {
         boolean userWantsConsole = doesUserWantConsole(args);
         String destinationFile = getDestinationFile(args);
         
-        int result = doComputation(args);
+        int result = doComputation(userWantsConsole, destinationFile, args);
 
         writeThing(userWantsConsole, destinationFile, result);
     } // main
