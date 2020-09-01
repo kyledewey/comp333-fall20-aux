69530560f541f5987d6377c0113aa2099f296e93
Kyle Dewey <kyle.dewey@csun.edu>
2020-09-01 13:35:53 -0700

    added an object; still have some issues

--- section1/virtual_dispatch/Temp.java
+++ section1/virtual_dispatch/Temp.java
@@ -3,42 +3,22 @@ import java.io.FileOutputStream;
 
 // java Temp foo bar baz
 // args = { "foo", "bar", "baz" }
-public class Temp {
-    // Issues:
-    // 1.) userWantsConsole and destinationFile keep getting repeated and passed around
-    // 2.) userWantsConsole and destinationFile are closely related
-    // 3.) userWantsConsole and destinationFile are somewhat conflicting (destinationFile doesn't
-    //     make sense if userWantsConsole == true)
-    // 4.) Repeated closing and opening of file hurts performance
-    // 5.) Overwriting file each time we write right now
-    public static void printThing(final boolean userWantsConsole,
-                                  final String destinationFile,
-                                  final int thingToPrint) {
-        if (userWantsConsole) {
-            System.out.println(42);
-        } else {
-            final File file = new File(destinationFile);
-            final FileOutputStream stream = new FileOutputStream(file);
-            stream.writeln(42);
-            stream.close();
-        }
-    } // printThing
-    
-    public static int doComputation(final boolean userWantsConsole,
-                                    final String destinationFile,
+public class Temp {    
+    public static int doComputation(final OutputDestination destination,
                                     final String[] args) {
-        printThing(userWantsConsole, destinationFile, 42);
+        destination.printThing(42);
     } // doComputation
     
     public static void main(final String[] args) {
         final boolean userWantsConsole = doesUserWantConsole(args);
         final String destinationFile = getDestinationFile(args);
-
+        final OutputDestination destination = new OutputDestination(userWantsConsole, destinationFile);
+        
         // do some computation
-        final int result = doComputation(userWantsConsole, destinationFile, args);
+        final int result = doComputation(destination, args);
 
-        // result = 7; // error: result is final; cannot reassign
-
-        printThing(userWantsConsole, destinationFile, result);
+        destination.printThing(result);
+        
+        //printThing(userWantsConsole, destinationFile, result);
     } // main
 } // Temp
