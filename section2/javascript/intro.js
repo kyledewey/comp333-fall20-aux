b38e1ed0c24114c47f4599b571c4c29ba6d417ce
Kyle Dewey <kyle.dewey@csun.edu>
2020-10-08 15:31:43 -0700

    second section

--- section2/javascript/intro.js
+++ section2/javascript/intro.js
@@ -22,36 +22,74 @@ function baz() {
     return b;
 }
 
-function printAllLessThanFive(arr) {
+// higher-order functions: functions are data
+// something is a higher-order function if it treats functions
+// as data (assigns a function to a variable, takes a function
+// as a parameter, returns a function)
+function printAllMatchingCondition(arr, condition) {
     let index;
     for (index = 0; index < arr.length; index++) {
-        if (arr[index] < 5) {
-            // System.out.println(arr[index]);
+        if (condition(arr[index])) {
             console.log(arr[index]);
         }
     }
+}
+
+function printAllLessThanFive(arr) {
+    let foobar = (function (element) {
+        return element < 5;
+    });
+    
+    printAllMatchingCondition(arr, foobar);
 }
 
 function printAllNotEqualThisValue(arr, value) {
-    let index;
-    for (index = 0; index < arr.length; index++) {
-        // != has the same issue as ==
-        // !== behaves like ===
-        if (arr[index] !== value) {
-            // System.out.println(arr[index]);
-            console.log(arr[index]);
-        }
-    }
+    let f = function (e) {
+        return e !== value;
+    };
+    printAllMatchingCondition(arr, f);
 }
 
 function printAllGreaterThan(arr, value) {
-    let index;
-    for (index = 0; index < arr.length; index++) {
-        // != has the same issue as ==
-        // !== behaves like ===
-        if (arr[index] > value) {
-            // System.out.println(arr[index]);
-            console.log(arr[index]);
-        }
-    }
+    let f = function (e) {
+        return e > value;
+    };
+    printAllMatchingCondition(arr, f);
 }
+
+// Original version: no use of higher-order functions
+// function printAllLessThanFive(arr) {
+//     let index;
+//     for (index = 0; index < arr.length; index++) {
+//         if (arr[index] < 5) {
+//             // System.out.println(arr[index]);
+//             console.log(arr[index]);
+//         }
+//     }
+// }
+
+// Original version: no use of higher-order functions
+// function printAllNotEqualThisValue(arr, value) {
+//     let index;
+//     for (index = 0; index < arr.length; index++) {
+//         // != has the same issue as ==
+//         // !== behaves like ===
+//         if (arr[index] !== value) {
+//             // System.out.println(arr[index]);
+//             console.log(arr[index]);
+//         }
+//     }
+// }
+
+// Original version: no use of higher-order functions
+// function printAllGreaterThan(arr, value) {
+//     let index;
+//     for (index = 0; index < arr.length; index++) {
+//         // != has the same issue as ==
+//         // !== behaves like ===
+//         if (arr[index] > value) {
+//             // System.out.println(arr[index]);
+//             console.log(arr[index]);
+//         }
+//     }
+// }
