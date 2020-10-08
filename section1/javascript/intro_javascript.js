94008d84206ff84ec14149f595eb27656edda053
Kyle Dewey <kyle.dewey@csun.edu>
2020-10-08 13:52:09 -0700

    through today

--- section1/javascript/intro_javascript.js
+++ section1/javascript/intro_javascript.js
@@ -10,32 +10,69 @@ function bar(x, y) {
     return x + y;
 }
 
-function printAllLessThanFive(arr) {
+// higher-order function, in contrast to first-order functions
+// distinction: is a function data?
+function printAllMatchingCondition(arr, condition) {
     let index;
     for (index = 0; index < arr.length; index++) {
-        if (arr[index] < 5) {
-            // System.out.println(arr[index]);
+        if (condition(arr[index])) {
             console.log(arr[index]);
         }
     }
 }
 
+function isLessThanFive(e) {
+    return e < 5;
+}
+
+function printAllLessThanFive(arr) {
+    printAllMatchingCondition(arr, isLessThanFive);
+    // let condition = function (e) {
+    //     return e < 5;
+    // };
+    // printAllMatchingCondition(arr, condition);
+    //printAllMatchingCondition(arr, function (e) { return e < 5; });
+}
+
+// Original version - doesn't use printAllMatchingCondition
+// function printAllLessThanFive(arr) {
+//     let index;
+//     for (index = 0; index < arr.length; index++) {
+//         if (arr[index] < 5) {
+//             // System.out.println(arr[index]);
+//             console.log(arr[index]);
+//         }
+//     }
+// }
+
 function printEverythingThatIsNotThisString(arr, thisString) {
-    let index;
-    for (index = 0; index < arr.length; index++) {
-        if (arr[index] !== thisString) {
-            // System.out.println(arr[index]);
-            console.log(arr[index]);
-        }
-    }
+    let f = function (foobar) {
+        return foobar !== thisString;
+    };
+    printAllMatchingCondition(arr, f);
+    // let index;
+    // for (index = 0; index < arr.length; index++) {
+    //     if (arr[index] !== thisString) {
+    //         // System.out.println(arr[index]);
+    //         console.log(arr[index]);
+    //     }
+    // }
 }
 
 function printAllGreaterThanValue(arr, value) {
-    let index;
-    for (index = 0; index < arr.length; index++) {
-        if (arr[index] > value) {
-            // System.out.println(arr[index]);
-            console.log(arr[index]);
-        }
-    }
+    let f = function (element) {
+        return element > value;
+    };
+    //foobar(27);
+    printAllMatchingCondition(arr, f);
+    // let index;
+    // for (index = 0; index < arr.length; index++) {
+    //     if (arr[index] > value) {
+    //         // System.out.println(arr[index]);
+    //         console.log(arr[index]);
+    //     }
+    // }
 }
+
+// global variable
+//f = "blah";
