d80882a35038288c839823ddf741c2494353754f
Kyle Dewey <kyle.dewey@csun.edu>
2020-10-15 14:46:00 -0700

    added examples with filter and reduce

---
+++ section2/javascript/filter_example.js
@@ -0,0 +1,10 @@
+// arr.filter(e => e > 3)
+
+function doSomethingWith(e) {
+    console.log(e);
+    globalVariable = e;
+    return true;
+}
+
+// var arr = [1, 2, 3, 4];
+// arr.filter(doSomethingWith)
---
+++ section2/javascript/reduce_example.js
@@ -0,0 +1,47 @@
+// Produce the smallest element in arr.  You may assume
+// that no element is greater than 0 (hint: use reduce).
+function findSmallest(arr) {
+    let result = 0;
+    let index;
+    for (index = 0; index < arr.length; index++) {
+        result = (arr[index] < result) ? arr[index] : result;
+        // if (arr[index] < result) {
+        //     result = arr[index];
+        // }
+    }
+    return result;
+}
+
+function findSum(arr) {
+    let result = 0;
+    let index;
+    for (index = 0; index < arr.length; index++) {
+        result = result + arr[index];
+    }
+    return result;
+}
+
+function findSum2(arr) {
+    let reducer = function (result, currentValue) {
+        return result + currentValue;
+    };
+    return arr.reduce(reducer, 0);
+}
+
+function findSum3(arr) {
+    //return arr.reduce((result, currentValue) => result + currentValue, 0);
+    return arr.reduce((accum, e) => accum + e, 0);
+}
+
+function appendAllAsStrings(arr) {
+    let result = "";
+    let index;
+    for (index = 0; index < arr.length; index++) {
+        result = result + arr[index].toString();
+    }
+    return result;
+}
+
+function appendAllAsStrings2(arr) {
+    return add.reduce((accum, e) => accum + e.toString(), "");
+}
