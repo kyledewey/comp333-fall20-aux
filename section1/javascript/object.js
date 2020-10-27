991e35e1c434e3c47ff1ea503c56efe9722867b0
Kyle Dewey <kyle.dewey@csun.edu>
2020-10-27 14:00:28 -0700

    covered constructors and prototype

--- section1/javascript/object.js
+++ section1/javascript/object.js
@@ -1,3 +1,12 @@
+// constructor
 function MyObject(param) {
-    this.foo = param;
+    // by using the word `new`, `this` is initially bound to a new empty object
+    // WITHOUT `new`, `this` is bound to the global object
+    // as a method call (obj.method), `this` is bound to `obj`
+    this.param = param;
 }
+MyObject.prototype.getParam = function() {
+    return this.param;
+}
+
+    
