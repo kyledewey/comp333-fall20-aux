9de3769e0037f0d712b116fddbd66b0df6900e03
Kyle Dewey <kyle.dewey@csun.edu>
2020-11-05 15:21:34 -0800

    through today

--- section2/swift/introduction.swift
+++ section2/swift/introduction.swift
@@ -1,13 +1,4 @@
-// a: Int is the same thing as "int a"
-// b: Int is the same thing as "int b"
-// Swift: local type inference (usually what languages have)
-//    - Has subtyping
-// Java:
-//    Object x = "SomeString";
-//    what is the type of x? - String, Object
-// Haskell: global type inference
-//    - Does NOT have subtyping
-func subtract(a: Int, b: Int) -> Int {
+/*func subtract(a: Int, b: Int) -> Int {
     // temp: Int
     // let temp = a - b;
     // type inference ^
@@ -41,3 +32,71 @@ func subtract3(_ first: Int, _ second: Int) -> Int {
 }
 
 print(subtract3(3, 1))
+*/
+
+func sub(_ a: Int, _ b: Int) -> Int {
+    return a - b;
+} 
+
+func callOther(f: () -> Int) -> Int {
+    return f()
+}
+
+func returns7() -> Int { return 7; }
+
+//print(callOther(f: returns7))
+
+
+func callForward(f: (Int, Int) -> Int, param: Int) -> Int {
+    return f(param, param)
+}
+
+func plus(a: Int, b: Int) -> Int { return a + b; }
+
+/*print(callForward(f: plus, param: 2))
+print(callForward(f: +, param: 3))
+print(callForward(f: -, param: 4))
+print(callForward(f: { (a, b) in a * b }, param: 5))*/
+
+// function makeAddSomething(param) {
+//   return otherParam => param + otherParam;
+// }
+// --OR--
+// function makeAddSomething(param) {
+//   return function (otherParam) {
+//     return param + otherParam;
+//   }
+// }
+
+/*
+func makeAddSomething(param: Int) -> ((Int) -> Int) {
+    return { otherParam in param + otherParam }
+}
+
+let f1 = makeAddSomething(param: 1);
+let f2 = makeAddSomething(param: 2);
+print(f1(5));
+print(f2(5));
+*/
+
+func callsFunc(f: (Int) -> Int, i: Int) -> Int {
+  let temp = f(i)
+  return temp
+}
+
+print(callsFunc(f: { a in 1 + a }, i: 5))
+
+//let temp = { a in a };
+let temp = { (a: Double) in a + 1 };
+temp(4.3);
+//let temp = 1 + 2.2;
+//print(temp)
+
+let t: Bool = true;
+let f = false;
+
+if 1 > 0 {
+    print("foobar");
+} else {
+    print("baz");
+}
