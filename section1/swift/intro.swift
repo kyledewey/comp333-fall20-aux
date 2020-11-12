32c2398af0a9c09cad8d36ebdca8a2358f392294
Kyle Dewey <kyle.dewey@csun.edu>
2020-11-12 13:49:30 -0800

    added code from today

--- section1/swift/intro.swift
+++ section1/swift/intro.swift
@@ -9,3 +9,33 @@ func subtract(_ first: Int, _ second: Int) -> Int {
 }
 
 print(subtract(2, 1))
+
+func add(_ leftToAdd: Int, _ rightToAdd: Int) -> Int {
+    return leftToAdd + rightToAdd;
+}
+
+add(2, 3)
+
+func sub(a: Int, b: Int) -> Int { return a - b; }
+
+func callsAnother(f: (Int, Int) -> Int, first: Int, second: Int) -> Int {
+    return f(first, second);
+}
+
+print(callsAnother(f: add, first: 7, second: 3))
+print(callsAnother(f: +, first: 8, second: 1))
+print(callsAnother(f: sub, first: 2, second: 1))
+
+func returnsAddSomething(first: Int) -> ((Int) -> Int) {
+    return { b in first + b };
+}
+
+print(callsAnother(f: { (a, b) in a * b }, first: 2, second: 3))
+
+// { (a, b) in a * b }
+// (a, b) => a * b
+// function (a, b) { return a * b; }
+
+
+// 1 + 2
+// +(1, 2)
