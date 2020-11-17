4a5007514446c37e396d970c31e69e8942a8aca6
Kyle Dewey <kyle.dewey@csun.edu>
2020-11-17 14:05:03 -0800

    code from class today

--- section1/swift/intro.swift
+++ section1/swift/intro.swift
@@ -39,3 +39,52 @@ print(callsAnother(f: { (a, b) in a * b }, first: 2, second: 3))
 
 // 1 + 2
 // +(1, 2)
+
+func takesFunctionWithNoArgs(f: () -> Int) -> Int {
+    //return f();
+    return 42;
+}
+
+func infiniteLoop() -> Int {
+    while true {}
+    return 7;
+}
+
+//let a: (() -> Int) -> Int = takesFunctionWithNoArgs;
+//takesFunctionWithNoArgs(f: { 2 + 2 });
+
+print(takesFunctionWithNoArgs(f: { 1 + 1 }))
+print(takesFunctionWithNoArgs(f: infiniteLoop));
+
+//print(takesFunctionWithNoArgs(f: 1 + 1))
+
+let b1: Bool = false;
+if (b1) {
+    print("in true");
+} else {
+    print("in false");
+}
+let temp: Int = (b1) ? 72 : 89;
+print(temp);
+
+func returnsNothing(x: Int) {
+    print(x);
+}
+
+func returnsTuple() -> (Int, String, Bool) {
+    let temp = (17, "foo", true);
+    case let (a, b, c) = temp;
+}
+
+func indirectIf(_ a: Bool, ifTrue: () -> Int, ifFalse: () -> Int) -> Int {
+    return (a) ? ifTrue() : ifFalse();
+    /*
+	if a {
+		return ifTrue();
+	} else {
+		return ifFalse();
+	}*/
+}
+
+print(indirectIf(true, ifTrue: { 1 + 1 }, ifFalse: { 2 + 2 })) // should be 2
+print(indirectIf(false, ifTrue: { 3+3 }, ifFalse: { 4 + 4 })) // should be 8
